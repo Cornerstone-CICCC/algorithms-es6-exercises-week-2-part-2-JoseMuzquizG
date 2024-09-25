acccf6a53a2b5884d0237bfdae73978b52a4f978
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-25 11:25:40 -0700

    Completed exercise 1

--- ex01.js
+++ ex01.js
@@ -1,2 +1,6 @@
 // Sort Exercise
 // Instructions: Create an array of at least five numbers in random order. Write the code to sort this array in ascending order.
+
+const numbers = [2, 3, 51, 9, 1, 6]
+numbers.sort((a, b) => a - b)
+console.log(numbers)
