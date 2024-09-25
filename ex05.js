27435be6d861a09f69f827a1ee802f322f075013
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-25 11:53:00 -0700

    Completed exercise 5

--- ex05.js
+++ ex05.js
@@ -1,3 +1,10 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const numbers = [1, 3, 51, 11, 35, 6]
+const pairNum = numbers.some(numbers => (numbers % 2) === 0)
+if (pairNum) {
+    console.log("There's at least one par number")
+} else {
+    console.log("There are no par numbers")
+}
