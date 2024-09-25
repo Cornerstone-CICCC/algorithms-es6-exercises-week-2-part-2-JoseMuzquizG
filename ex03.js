b0631d5946d37cd32fe1c75d20e6cf9b59b9dc97
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-25 11:36:00 -0700

    Completed exercise 3

--- ex03.js
+++ ex03.js
@@ -1,3 +1,7 @@
 // Reverse Exercise
 // Instructions: Create an array of at least five numbers. Write code to reverse the order of the elements in the array.
 
+const numbers = [2, 3, 51, 11, 34, 6]
+let reversed = [...numbers].reverse()
+console.log(numbers)
+console.log(reversed)
