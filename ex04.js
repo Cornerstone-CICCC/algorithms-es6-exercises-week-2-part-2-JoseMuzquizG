51c665f760bf64097b572a8c807cc7a31db5905b
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-25 11:45:52 -0700

    Completed exercise 4

--- ex04.js
+++ ex04.js
@@ -1,3 +1,12 @@
 // Includes Exercise
 // Instructions: Create an array of at least five different fruits as strings. Write code to check if the array includes the fruit "apple".
 
+const fruits = ["banana", "grape", "watermelon", "strawberry", "raisin", "cranberry", "apple"]
+function appleJuice(fruits) {
+    if (fruits.includes("apple")) {
+        return "Let's make apple juice ☺"
+    } else {
+        return "There won't be any juice today"
+    }
+}
+console.log(appleJuice(fruits))
